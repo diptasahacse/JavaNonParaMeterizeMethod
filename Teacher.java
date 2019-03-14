adbf85425a7e03d626e032a78b84cb2ef66220a3
Dipta Saha <32943396+diptasahacse@users.noreply.github.com>
2019-03-14 18:33:39 +0530

    Create a Class which is use from another class

---
+++ Teacher.java
@@ -0,0 +1,13 @@
+public class Teacher {
+    int age,salary;
+    String name,dept;
+    
+    void Display()
+    {
+        System.out.println("Name = "+name);
+        System.out.println("Age = "+age);
+        System.out.println("Dept = "+dept);
+        System.out.println("Salary = "+salary);
+    }
+    
+}
