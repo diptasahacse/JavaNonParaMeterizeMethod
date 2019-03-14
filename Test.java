e183bab8094e0ced33630e130580d07acae30c9e
Dipta Saha <32943396+diptasahacse@users.noreply.github.com>
2019-03-14 18:36:00 +0530

    CreateobjectwhichiscontainTeacherclassFeatures

---
+++ Test.java
@@ -0,0 +1,32 @@
+
+package NonParameterizeMethod;
+
+public class Test {
+    public static void main(String[] args) {
+        Teacher teacher1 = new Teacher();
+        teacher1.age = 20;
+        teacher1.salary = 10000;
+        teacher1.name = "Dipta Saha";
+        teacher1.dept = "CSE";
+        teacher1.Display();
+        
+        
+        
+        Teacher teacher2 = new Teacher();
+        teacher2.age = 30;
+        teacher2.salary = 110000;
+        teacher2.name = "Keya Saha";
+        teacher2.dept = "Political Science";
+        teacher1.Display();
+       
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
